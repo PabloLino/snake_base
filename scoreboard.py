ce2503d73b149d93a02dcc390e12f08cfa3005d2
Pablo Lino <118943316+PabloLino@users.noreply.github.com>
2024-10-03 16:01:05 -0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 import time
+import os
+import sys
 
 ALIGNMENT = 'center'
 FONT = ('Courier', 15, 'bold')
@@ -11,14 +13,24 @@ class Scoreboard(Turtle):
         super().__init__()
         self.hideturtle()
         self.current_score = 0
-        with open("data.txt") as data:
-            self.highest_score = int(data.read())
+        self.load_high_score()
         self.penup()
+
+    def load_high_score(self):
+        if getattr(sys, 'frozen', False):
+            # Estamos em um executável
+            data_file = os.path.join(sys._MEIPASS, 'data.txt')
+        else:
+            # Estamos em um ambiente normal
+            data_file = 'data.txt'
+
+        with open(data_file) as data:
+            self.highest_score = int(data.read())
 
     def refresh(self):
         self.clear()
         self.goto(0, 270)
-        with open("data.txt") as data:
+        with open(self.get_data_file_path()) as data:
             self.highest_score = int(data.read())
         self.write(arg=f'Score: {self.current_score} | High Score: {self.highest_score}', align=ALIGNMENT, font=FONT)
 
@@ -26,7 +38,12 @@ class Scoreboard(Turtle):
         self.goto(0, 0)
         self.write(arg='GAME OVER', align=ALIGNMENT, font=FONT)
         if self.current_score > self.highest_score:
-            with open("data.txt", mode='w') as data:
+            with open(self.get_data_file_path(), mode='w') as data:
                 data.write(f'{self.current_score}')
         time.sleep(3)
         self.current_score = 0
+
+    def get_data_file_path(self):
+        if getattr(sys, 'frozen', False):
+            return os.path.join(sys._MEIPASS, 'data.txt')
+        return 'data.txt'
